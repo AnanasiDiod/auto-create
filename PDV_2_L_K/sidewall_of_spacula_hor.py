4a79ab0c1c774d42fba325c8342b30cd1988c3e2
AnanasiDiod <61230551+AnanasiDiod@users.noreply.github.com>
2023-04-05 22:47:24 +0300

    final revision + exe

--- PDV_2_L_K/sidewall_of_spacula_hor.py
+++ PDV_2_L_K/sidewall_of_spacula_hor.py
@@ -18,19 +18,12 @@ def main(width, heigh, quantity, nw, np, path=str()):
     p.go_line(-14.35, 14)
     p.go_init()
 
-    if width > 100:
-        p.set_xy(6.1, -16)
-        dw = (width - 114.4)/(nw - 1)
-        for i in range(nw):
-            p.circle(0, 0, 4.9 / 2)
-            p.circle(43.48, 0, 4.9/2)
-            p.set_xy(6.1, -(6 + (i + 1) * dw))
-    else:
-        p.set_xy(6.1, -2.14)
-        p.circle(0, 0, 2.45)
-        p.circle(43.48, 0,  2.45)
-        p.circle(0, -14, 2.45)
-        p.circle(-43.48, 0,  2.45)
+    p.set_xy(6.1, -16)
+    dw = (width - 114.4)/(nw - 1)
+    for i in range(nw):
+        p.circle(0, 0, 4.9 / 2)
+        p.circle(43.48, 0, 4.9/2)
+        p.set_xy(6.1, -(16 + (i + 1) * dw))
 
     doc.saveas(path+"Боковина лопатки горизонтальная " + str(width) + 'x' + str(heigh) +
                ' 0,8мм ' + str(quantity * 2 * np)+'шт.dxf')
