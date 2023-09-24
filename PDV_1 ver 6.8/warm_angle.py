62121bd99a378dc48ad984a017b080bbd245ccd2
AnanasiDiod <61230551+AnanasiDiod@users.noreply.github.com>
2023-09-24 12:43:29 +0300

    warm + exe

--- PDV_1 ver 6.8/warm_angle.py
+++ PDV_1 ver 6.8/warm_angle.py
@@ -35,8 +35,25 @@ def irregular_150(width, heigh, quantity, path=str()):
     p = point()
     p.msp = msp
 
+    p.go_line(17.65, 0)
+    p.go_line(16.54, -16.54)
+    p.go_line(16.54, 16.54)
+    p.go_line(17.65, 0)
+    p.go_line(0, -105.04)
+    p.go_line(-17.65, 0)
+    p.go_line(-16.54, 16.54)
+    p.go_line(-16.54, -16.54)
+    p.go_line(-17.65, 0)
+    p.go_init()
+    p.circle(6, -25.37, 4.9 / 2)
+    p.circle(56.37, 0, 4.9 / 2)
+    p.circle(0, -54.3, 4.9 / 2)
+    p.circle(-56.37, 0, 4.9 / 2)
+    p.set_xy(0, 0)
+    p.circle(48.57, -52.47, 22 / 2)
+
     doc.saveas(path + "Греющий угол " + str(width) + 'x' + str(heigh) +
-               ' 0,8мм ' + str(quantity * 4) + 'шт.dxf')
+               ' 0,8мм ' + str(quantity) + 'шт.dxf')
 
 
 def irregular_200(width, heigh, quantity, path=str()):
@@ -45,6 +62,25 @@ def irregular_200(width, heigh, quantity, path=str()):
     msp = doc.modelspace()
     p = point()
     p.msp = msp
+
+    p.go_line(17.65, 0)
+    p.go_line(16.54, -16.54)
+    p.go_line(16.54, 16.54)
+    p.go_line(17.65, 0)
+    p.go_line(0, -42.78)
+    p.go_line(-5.8, -3.11)
+    p.go_line(0, -13.16)
+    p.go_line(5.8, -3.11)
+    p.go_line(0, -42.88)
+    p.go_line(-17.65, 0)
+    p.go_line(-16.54, 16.54)
+    p.go_line(-16.54, -16.54)
+    p.go_line(-17.65, 0)
+    p.go_init()
+    p.circle(6, -25.37, 4.9 / 2)
+    p.circle(56.37, 0, 4.9 / 2)
+    p.circle(0, -54.3, 4.9 / 2)
+    p.circle(-56.37, 0, 4.9 / 2)
 
     doc.saveas(path + "Греющий угол " + str(width) + 'x' + str(heigh) +
                ' 0,8мм ' + str(quantity) + 'шт.dxf')
--- PDV_1 ver 6.8/warm_vetical_with_hole.py
+++ PDV_1 ver 6.8/warm_vetical_with_hole.py
@@ -44,16 +44,26 @@ def irregular_150(width, heigh, quantity, path=str()):
     p = point()
     p.msp = msp
 
-    doc.saveas(path + "Греющий верт с отверстием " + str(width) + 'x' + str(heigh) +
-               ' 0,8мм ' + str(quantity) + 'шт.dxf')
-
-
-def irregular_200(width, heigh, quantity, path=str()):
-    doc = dxf.new()
-    doc.layers.add("FIGURE", color=2)
-    msp = doc.modelspace()
-    p = point()
-    p.msp = msp
+    p.go_line(137.96, 0)
+    p.go_line(0, -7)
+    p.go_line(-15, 0)
+    p.go_line(0, -132)
+    p.go_line(15, 0)
+    p.go_line(0, -7)
+    p.go_line(-60.5, 0)
+    p.go_line(-3.3, 4)
+    p.go_line(-10.37, 0)
+    p.go_line(-3.3, -4)
+    p.go_line(-60.5, 0)
+    p.go_line(0, 7)
+    p.go_line(15, 0)
+    p.go_line(0, 132)
+    p.go_line(-15, 0)
+    p.go_init()
+    p.circle(41.88, -6.8, 4.9/2)
+    p.circle(54.2, 0, 4.9 / 2)
+    p.circle(0, -132.4, 4.9/2)
+    p.circle(-54.2, 0, 4.9 / 2)
 
     doc.saveas(path + "Греющий верт с отверстием " + str(width) + 'x' + str(heigh) +
                ' 0,8мм ' + str(quantity) + 'шт.dxf')
@@ -62,7 +72,5 @@ def irregular_200(width, heigh, quantity, path=str()):
 def main(width, heigh, quantity, path=str()):
     if heigh <= 150:
         irregular_150(width, heigh, quantity, path)
-    elif heigh <= 200:
-        irregular_200(width, heigh, quantity, path)
     else:
         regular(width, heigh, quantity, path)
